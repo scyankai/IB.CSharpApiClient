39af8a56a00491c2b774b1bdb0a9ec2bb1e3680a
yankai <msyankai@live.cn>
2023-04-14 16:18:29 +0800

    remove name param

--- src/IB.CSharpApiClient/IBClientParameters.cs
+++ src/IB.CSharpApiClient/IBClientParameters.cs
@@ -10,8 +10,6 @@ namespace IB.CSharpApiClient
 
         public int Port { get; set; }
 
-        public string Name { get; set; }
-
         public int ClientId { get; set; }
 
         public int interval { get; set; } = 60 * 1000;
