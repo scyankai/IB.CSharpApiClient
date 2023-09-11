c8e17a5be1ced7a1c438ad9e06281991d0593e29
yankai <msyankai@live.cn>
2023-09-11 19:55:53 +0800

    update IB API  to 10.24

--- MarketDataService/Program.cs
+++ MarketDataService/Program.cs
@@ -1,33 +1,69 @@
 ﻿using IB.CSharpApiClient;
+using IBApi;
 
 var client = IBClientFactory.CreateNew();
-client.Parameters = new IBClientParameters { Host = "127.0.0.1", Port = 7497, ClientId = 5 };
+client.Parameters = new IBClientParameters { Host = "127.0.0.1", Port = 7490, ClientId = 5 };
 
-client.CurrentTime += Client_CurrentTime;
+client.OpenOrder += Client_OpenOrder;
+client.NextValidId += HandleConnected;
+client.OrderStatus += Client_OrderStatus;
 
-void Client_CurrentTime(IB.CSharpApiClient.Messages.CurrentTimeMessage obj)
-{
-    Console.WriteLine(obj.Time.ToString());
-}
+_ = client.AutoConnect();
+
+client.ReqIds(0);
 
 while (true)
 {
-    client.ReqCurrentTime();
-    Console.WriteLine("client.ReqCurrentTime");
-    //await Task.Delay(1000);
+    Console.ReadLine();
+}
 
 
+void Client_OrderStatus(IB.CSharpApiClient.Messages.OrderStatusMessage obj)
+{
+    Console.WriteLine("Client_OrderStatus");
+}
 
-    var input = Console.ReadLine();
+void Client_OpenOrder(IB.CSharpApiClient.Messages.OpenOrderMessage obj)
+{
+    Console.WriteLine("Client_OpenOrder");
+}
 
-    if (input == "1")
+void HandleConnected(IB.CSharpApiClient.Messages.NextValidIdMessage obj)
+{
+
+    Console.WriteLine("connected");
+
+    client.ReqAllOpenOrders();
+
+    return;
+
+    while (true)
     {
-        _ = client.AutoConnect();
-        Console.WriteLine("client.Connect");
-    }
-    if (input == "2")
-    {
-        client.Disconnect();
-        Console.WriteLine("client.Disconnect");
+        client.ReqCurrentTime();
+        Console.WriteLine("client.ReqCurrentTime");
+        //await Task.Delay(1000);
+
+        var input = Console.ReadLine();
+
+        if (input == "1")
+        {
+            _ = client.AutoConnect();
+            Console.WriteLine("client.Connect");
+        }
+        if (input == "2")
+        {
+            client.Disconnect();
+            Console.WriteLine("client.Disconnect");
+        }
+        if (input == "3")
+        {
+            Console.WriteLine("ReqAllOpenOrders");
+            client.ReqAllOpenOrders();
+        }
+        if (input == "4")
+        {
+            Console.WriteLine("ReqAllOpenOrders");
+            client.ReqAllOpenOrders();
+        }
     }
 }
